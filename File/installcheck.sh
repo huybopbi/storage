eb42263d0bd2295cfd4186fdd06ee284703ccae2
Phạm Quang Huy <39458058+huybopbi@users.noreply.github.com>
2023-05-18 17:25:41 +0900

    Update installcheck.sh

--- File/installcheck.sh
+++ File/installcheck.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/huybopbi/storage/master/File/check.sh
 chmod +x check.sh
 bash check.sh
-@daily /path/to/check_vps_status.sh >/dev/null 2>&1
+0 6 * * * /path/to/check.sh >/dev/null 2>&1
